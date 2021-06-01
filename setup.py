dc8e2e1eb92f6ea6a67e2d319092eae796eb5203
Shigure_Hotaru <dilbert_don@qq.com>
2021-06-01 10:21:34 +0800

    2021.06.01 Initialization

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2021/6/1 10:23
+# @Author   : Shigure_Hotaru
+# @Email    : minjie96@sencyber.cn
+# @File     : setup.py
+# @Version  : Python 3.8.5 +
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sencyber-tools-minjie96",
+    version="0.0.1",
+    author="minjie96",
+    author_email="minjie96@sencyber.cn",
+    description="Sencyber Tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
